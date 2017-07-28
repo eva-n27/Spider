3f5c52bebdb029d7e567c595cfd8c2617182daec
Zhengpeng Xiang <xzp24678@hit.edu.cn>
2017-07-28 14:30:55 +0800

    Revert "修改类的实现"
    This reverts commit aac38fd2d7a8d31ca5029b2e234041990bd01771.

--- baidu/search.py
+++ baidu/search.py
@@ -2,56 +2,49 @@
 # author: Zhengpeng Xiang
 # date: 2017/07/26
 
-
 import requests
 from bs4 import BeautifulSoup as bs
 
 
 class Spider:
-    """
-    function:
-        - 爬虫类，用于将爬取百度搜索的结果爬取下来。
-        - 给Spider的实例s传入keywords, number_of_results之后就可以自动爬取，使用get_result方法获得爬取得结果
-        - 使用run方法重复使用实例s，可传递（number_of_pages_stored_, text_, html_, number_of_results_, keywords_）等参数
-    input:
-        - 使用run()方法进行传递
-        - keywords 搜索的关键词，字符串
-        - number_of_results 搜索的结果数，整数
-    output:
-        - text 使用get_result方法获得,一维list，每一个元素对应一个搜索结果中的文本
-    """
-    def __init__(self):
-        # 输入
-        self.keywords = ''  # 搜索的关键词
-        self.number_of_results = 0  # 指定搜索结果的数量
-
+    def __init__(self, keywords, number_of_results):
+        """
+        :param keywords: 搜索的关键词
+        :param number_of_results: 搜索的结果数量
+        """
         # 基本属性
         self.text = []  # 爬取结果
         self.html = []  # 每一次爬取到的页面
+        self.keywords = keywords  # 搜索的关键词
+        self.number_of_results = number_of_results  # 指定搜索结果的数量
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0 cb) like Gecko'}  # 请求Header
         self.number_of_pages_stored = 0  # 已经爬取过的页数
+        print self.number_of_pages_stored
+        # 运行
+        self.run()
 
     def get_html(self, number_of_pages):
         """
-        爬取html
+        from https://github.com/fancoo/BaiduCrawler/blob/master/baidu_crawler.py
+        :return:
         """
-        html = []
+        html_ = []
 
         # 抓取数据内容
         for i in xrange(number_of_pages):
             key = {'wd': self.keywords, 'pn': (i + self.number_of_pages_stored) * 10}  # pn表示页数
             web_content = requests.get("https://www.baidu.com/s?", params=key, headers=self.headers, timeout=4)
             print web_content.url
-            html.append(web_content.text)
+            html_.append(web_content.text)
 
         print "html下载完成！"
-        return html
+        return html_
 
     def html_parser(self):
         """
         解析html
         """
-        text = []
+        text_ = []
         for i in xrange(len(self.html)):
             soup = bs(self.html[i], 'lxml').find('div', id="content_left")
 
@@ -63,18 +56,19 @@ class Spider:
             # 提取数据
             if len(c_abstract) != 0:
                 for item_ in c_abstract:
-                    text.append(item_.text.strip())
+                    text_.append(item_.text.strip())
         print "html解析完成！"
-        return text
+        return text_
 
-    def run(self, keywords, number_of_results):
+    def run(self, number_of_pages_stored_=None, text_=None, html_=None, number_of_results_=None, keywords_=None):
         """
         爬取指定数目的百度搜索结果
         """
-        self.clean()
-
-        self.keywords = keywords
-        self.number_of_results = number_of_results
+        if number_of_pages_stored_ is not None or text_ is not None or html_ is not None or \
+           number_of_results_ is not None or keywords_ is not None:
+            # 设置参数
+            self.reset(number_of_pages_stored_=number_of_pages_stored_, text_=text_, html_=html_,
+                       number_of_results_=number_of_results_, keywords_=keywords_)
 
         # 爬取百度的搜索结果
         while len(self.text) < self.number_of_results:
@@ -98,15 +92,36 @@ class Spider:
         print "从百度搜索结果中共爬取%s个结果，爬取完成！" % self.number_of_results
         return self
 
-    def clean(self):
+    def reset(self, number_of_pages_stored_=None, text_=None, html_=None, number_of_results_=None, keywords_=None):
         """
-        将类中的属性值清空
+        设置类中的属性值
         """
-        self.text = []
-        self.html = []
-        self.number_of_pages_stored = 0
+        if number_of_pages_stored_ is not None:
+            self.number_of_pages_stored = number_of_pages_stored_
+        else:
+            self.number_of_pages_stored = 0
 
-        print "参数清空完毕！"
+        if text_ is not None:
+            self.text = text_
+        else:
+            self.text = []
+
+        if html_ is not None:
+            self.html = html_
+        else:
+            self.html = []
+
+        if number_of_results_ is not None:
+            self.number_of_results = number_of_results_
+        else:
+            self.number_of_results = 0
+
+        if keywords_ is not None:
+            self.keywords = keywords_
+        else:
+            self.keywords = ''
+
+        print "参数重置完毕！"
         return self
 
     def get_result(self):
@@ -116,15 +131,14 @@ class Spider:
         """
         return self.text
 
-
 if __name__ == '__main__':
-    s = Spider()
-    text = s.run('游戏名字', 20).get_result()
+    s = Spider('游戏名字', 20)
+    text = s.get_result()
     for item in text:
         print item
 
     print '******************'
 
-    text = s.run(keywords='游戏名字', number_of_results=10).get_result()
+    text = s.run(keywords_='游戏名字', number_of_results_=10).get_result()
     for item in text:
         print item
